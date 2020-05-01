213a505261b34088775caf640c68bf776a701f3d
cecabrera <cecabrera55@gmail.com>
2020-05-01 13:56:19 -0500

    adición del commit

---
+++ commit.R
@@ -0,0 +1,7 @@
+require(rmarkdown)
+system("git status")
+system('git config --global user.email "cecabrera55@gmail.com"')
+system('git config --global user.name "Camilo"')
+system("git add * ")
+# system('git push origin master')
+
