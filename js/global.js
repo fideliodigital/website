13a0bb7c4e18d53eb219070a76f29c310763bef5
Camilo Cabrera <cecabrera55@gmail.com>
2025-04-04 19:49:17 -0500

    Modernize global.js to use ES6+ syntax

--- js/global.js
+++ js/global.js
@@ -1,9 +1,9 @@
 // Global Javascript Initialization
-var Global = function() {
+const Global = (() => {
   'use strict';
 
-  // Bootstra Components
-  var handleBootstrapComponents = function() {
+  // Bootstrap Components
+  const handleBootstrapComponents = () => {
     // Bootstrap Carousel
     $('.carousel').carousel({
       interval: 5000,
@@ -23,15 +23,15 @@ var Global = function() {
     $('.popovers-hide').popover('hide');
     $('.popovers-toggle').popover('toggle');
     $('.popovers-destroy').popover('destroy');
-  }
+  };
 
   // Scroll To Section
-  var handleScrollToSection = function() {
-    $(function() {
+  const handleScrollToSection = () => {
+    $(() => {
       $('a[href*=#js__scroll-to-]:not([href=#js__scroll-to-])').on('click', function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+          let target = $(this.hash);
+          target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
           if (target.length) {
             $('html,body').animate({
               scrollTop: target.offset().top - 90
@@ -41,82 +41,82 @@ var Global = function() {
         }
       });
     });
-  }
+  };
 
   // Handle Promo Section
-  var handlePromoSection = function() {
+  const handlePromoSection = () => {
     $('.js__fullwidth-img').each(function() {
-      $(this).css('background-image', 'url(' + $(this).children('img').attr('src') + ')');
+      $(this).css('background-image', `url(${$(this).children('img').attr('src')})`);
       $(this).children('img').hide();
     });
-  }
+  };
 
   // Handle Overlay
-  var handleOverlay = function() {
-    var overlay = $('.js__bg-overlay'),
-      headerOverlay = $('.js__header-overlay'),
-      trigger = $('.js__trigger');
+  const handleOverlay = () => {
+    const overlay = $('.js__bg-overlay');
+    const headerOverlay = $('.js__header-overlay');
+    const trigger = $('.js__trigger');
 
-    trigger.on('click', function() {
+    trigger.on('click', () => {
       overlay.toggleClass('-is-open');
       headerOverlay.toggleClass('-is-open');
       trigger.toggleClass('-is-active');
     });
-  }
+  };
 
   // Vertical Center Aligned
   // Note! This works only with promo block and background image via CSS.
-  var handleVerticalCenterAligned = function() {
-    $('.js__ver-center-aligned').each(function() {
-      $(this).css('padding-top', $(this).parent().height() / 2 - $(this).height() / 2);
-    });
-    $(window).resize(function() {
+  const handleVerticalCenterAligned = () => {
+    const centerElements = () => {
       $('.js__ver-center-aligned').each(function() {
         $(this).css('padding-top', $(this).parent().height() / 2 - $(this).height() / 2);
       });
-    });
-  }
+    };
+    
+    centerElements();
+    $(window).resize(centerElements);
+  };
 
   // handle group element heights
-  var handleEqualHeight = function() {
-   $('[data-auto-height]').each(function() {
-      var parent = $(this);
-      var items = $('[data-height]', parent);
-      var height = 0;
-      var mode = parent.attr('data-mode');
-      var offset = parseInt(parent.attr('data-offset') ? parent.attr('data-offset') : 0);
+  const handleEqualHeight = () => {
+    $('[data-auto-height]').each(function() {
+      const parent = $(this);
+      const items = $('[data-height]', parent);
+      let height = 0;
+      const mode = parent.attr('data-mode');
+      const offset = parseInt(parent.attr('data-offset') ? parent.attr('data-offset') : 0);
 
       items.each(function() {
-        if ($(this).attr('data-height') == "height") {
+        if ($(this).attr('data-height') === "height") {
           $(this).css('height', '');
         } else {
           $(this).css('min-height', '');
         }
 
-        var height_ = (mode == 'base-height' ? $(this).outerHeight() : $(this).outerHeight(true));
+        const height_ = (mode === 'base-height' ? $(this).outerHeight() : $(this).outerHeight(true));
         if (height_ > height) {
           height = height_;
         }
       });
 
-      height = height + offset;
+      const finalHeight = height + offset;
 
       items.each(function() {
-        if ($(this).attr('data-height') == "height") {
-          $(this).css('height', height);
+        if ($(this).attr('data-height') === "height") {
+          $(this).css('height', finalHeight);
         } else {
-          $(this).css('min-height', height);
+          $(this).css('min-height', finalHeight);
         }
       });
 
-      if(parent.attr('data-related')) {
+      if (parent.attr('data-related')) {
         $(parent.attr('data-related')).css('height', parent.height());
       }
-   });       
-  }
+    });       
+  };
 
   return {
-    init: function() {
+    init: () => {
       handleBootstrapComponents(); // initial setup for Bootstrap Components
       handleScrollToSection(); // initial setup for Scroll To Section
       handlePromoSection(); // initial setup for Promo Section
@@ -124,9 +124,10 @@ var Global = function() {
       handleVerticalCenterAligned(); // initial setup for Vertical Center Aligned
       handleEqualHeight(); // initial setup for Equal Height
     }
-  }
-}();
+  };
+})();
 
-$(document).ready(function() {
+// Document Ready
+$(() => {
   Global.init();
 });
